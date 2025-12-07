f905259475dcc6240c039c41b412df9a8ff6d664
Javier Valseca <javivals@ucm.es>
2025-12-07 21:04:38 +0100

    click

--- Scripts/MinigameManager.gd
+++ Scripts/MinigameManager.gd
@@ -4,6 +4,9 @@ class_name MinigameManager
 @export var all_minigames:Array[PackedScene]
 var available_games:Array[PackedScene]
 
+@export var animation_player:AnimationPlayer
+@export var transition_animations:Array[StringName]
+
 var speed:float = 1
 const MAX_SPEED := 1.75
 
@@ -13,6 +16,8 @@ func start():
 		while available_games.size() > 0:
 			var minigame:Minigame = spawn_random_game()
 			await minigame.on_finished
+			animation_player.play(transition_animations.pick_random())
+			await animation_player.animation_finished
 		accelerate()
 	win()
 
@@ -24,6 +29,7 @@ func spawn_random_game() -> Minigame:
 	var minigame:PackedScene = available_games.pick_random()
 	available_games.erase(minigame)
 	var minigame_instance:Minigame = minigame.instantiate()
+	add_child(minigame_instance)
 	return minigame_instance
 
 func accelerate():
