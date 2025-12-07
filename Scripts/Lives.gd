e764b9dae296c051105032e23554fd854ad1445b
MikalMica <mika@ucm.es>
2025-12-07 21:44:46 +0100

    lives

---
+++ Scripts/Lives.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var init_lives:int = 3
+var current_lives:int = init_lives
+
+signal no_lives_left
+
+func add_lives(lives:int):
+	current_lives += lives
+	update_lives()
+
+func add_live():
+	current_lives += 1
+	update_lives()
+
+func substract_live():
+	current_lives -= 1
+	update_lives()
+
+func substract_lives(lives:int):
+	current_lives -= lives
+	update_lives()
+
+func reset_lives():
+	current_lives = init_lives
+	update_lives()
+
+# Updates everything that needs to be updated when lives change
+func update_lives():
+	
+	if(current_lives <= 0):
+		no_lives_left.emit()
+		# SceneManager.change_scene([lose_scene_path]) 
