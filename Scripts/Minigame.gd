469ede8b1502fc5c5ea3f8f3f6a41735ea75cb52
Javier Valseca <javivals@ucm.es>
2025-12-07 20:40:07 +0100

    minigame manager in progress

--- Scripts/Minigame.gd
+++ Scripts/Minigame.gd
@@ -1,2 +1,10 @@
 extends Node
 class_name Minigame
+
+var success:bool
+
+func completed():
+	success = true
+
+func has_won():
+	return success
--- Scripts/MinigameManager.gd
+++ Scripts/MinigameManager.gd
@@ -1,2 +1,6 @@
 extends Node
 class_name MinigameManager
+
+@export var all_minigames:Array[PackedScene]
+var available_games:Array[PackedScene]
+var used_games:Array[PackedScene]
