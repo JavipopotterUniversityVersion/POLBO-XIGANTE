8bec926f62fd278771f63af5bc330e9c52d3eb25
MikalMica <mika@ucm.es>
2025-12-09 22:20:47 +0100

    lambada click

--- Scripts/PlayAnimation.gd
+++ Scripts/PlayAnimation.gd
@@ -1,7 +1,9 @@
 extends AnimatedSprite2D
 
 @export var animationName:String
+@export var idle:String
 
 func playAnim():
 	play(animationName)
-	await animation_finished
+	await animation_looped
+	play(idle)
